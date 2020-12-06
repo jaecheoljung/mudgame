1b5a4967497986e22dd3126bb14aaa6f46effe6e
DohyungKim <ehgud456456@naver.com>
2020-12-06 18:14:40 +0900

    깃 테스트용 블랭크 한줄 지웠습니다.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ const fs = require("fs");
 const mongoose = require("mongoose");
 const crypto = require("crypto");
 
-
 const { constantManager, mapManager } = require("./datas/Manager");
 const { Player } = require("./models/Player");
 const { secret, mongoURI } = require("../config");
 const { action } = require("./controller/action");
 const { signup } = require("./controller/sign");
 const { authentication } = require("./middle/auth");
-
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +17,6 @@ app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
 mongoose.connect( mongoURI, { useNewUrlParser: true, useUnifiedTopology: true } );
-
 
 app.get("/", (req, res) => {
   res.render("index", { gameName: constantManager.gameName });
